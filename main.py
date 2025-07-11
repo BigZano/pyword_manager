27b64bb4342475034a5af391fb2de928f6005aae
Bret Zanotelli <bret.zanotelli@yahoo.com>
2025-07-11 15:14:06 -0700

    user input down!

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ def add():
     username = entryName.get()
     password = entryPassword.get()
     if username and password:
-        with open("")
+        with open("passwords.txt", 'a') as f:
+            f.write(f"{username}:{password}\n")
+        messagebox.showinfo("Success", "Password added successfully!")
+    else:
+        messagebox.showwarning("Input Error", "Please enter both username and password.")
