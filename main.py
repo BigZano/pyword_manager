c2fb01eaf304bbc2a62834c9e02de83749cb8271
Bret Zanotelli <bret.zanotelli@yahoo.com>
2025-07-11 14:53:19 -0700

    Beginning boiler plate

---
+++ main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def add():
+    username = entryName.get()
+    password = entryPassword.get()
+    if username and password:
+        with open("")
